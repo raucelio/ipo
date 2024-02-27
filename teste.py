fc6b9ec234b383b67b5601338e09604075e82c29
raucelio.rccv <raucelio.rccv@gmail.com>
2024-02-27 19:17:03 -0300

    pandas instalado sqlite3 instalado criando o indicador transversal

--- teste.py
+++ teste.py
@@ -4,3 +4,56 @@
 
 import pandas as pd
 import sqlite3 as bd
+
+
+# indicadores transversais
+# Pr1-Transversal – PRISÕES E TERMOS CIRCUNSTANCIADOS DE OCORRÊNCIA
+# Pr2-Transversal – DESCAPITALIZAÇÃO
+# Pr3-Transversal – OPERAÇÕES DE POLÍCIA JUDICIÁRIA
+# Pr4-Transversal – BUSCA E APREENSÃO
+# Pr5-Transversal – OUTRAS MEDIDAS CAUTELARES DISTINTAS DA PRISÃO
+# Pr6-Transversal – INDICIAMENTOS
+# Pr7-Transversal – RELATÓRIOS FINAIS DE OPERAÇÕES DE POLÍCIA JUDICIÁRIA
+# Pr8-Transversal – COOPERAÇÃO INTERNACIONAL
+# Pr9-Transversal – APOIO OPERACIONAL (MOBILIZAÇÃO DE SERVIDORES)
+# Pr10-Transversal – APOIO OPERACIONAL (FORNECIMENTO DE INFORMAÇÕES)
+
+# Sigla
+# trvsl - transversal
+# pr    - produtividade
+# ps    - peso
+
+
+pr_trvsl_01 = 1
+ps_trvsl_01 = 1
+pr_trvsl_02 = 1
+ps_trvsl_02 = 1
+pr_trvsl_03 = 1
+ps_trvsl_03 = 1
+pr_trvsl_04 = 1
+ps_trvsl_04 = 1
+pr_trvsl_05 = 1
+ps_trvsl_05 = 1
+pr_trvsl_06 = 1
+ps_trvsl_06 = 1
+pr_trvsl_07 = 1
+ps_trvsl_07 = 1
+pr_trvsl_08 = 1
+ps_trvsl_08 = 1
+pr_trvsl_09 = 1
+ps_trvsl_09 = 1
+pr_trvsl_10 = 1
+ps_trvsl_10 = 1
+
+
+pr_trvsl = (
+             pr_trvsl_01 * ps_trvsl_01 + pr_trvsl_02 * ps_trvsl_02 + pr_trvsl_03 * ps_trvsl_03 +
+             pr_trvsl_04 * ps_trvsl_04 + pr_trvsl_05 * ps_trvsl_05 + pr_trvsl_06 * ps_trvsl_06 +
+             pr_trvsl_07 * ps_trvsl_07 + pr_trvsl_08 * ps_trvsl_08 + pr_trvsl_09 * ps_trvsl_09 +
+             pr_trvsl_10 * ps_trvsl_10
+            )
+                /
+           (
+             ps_trvsl_01 + ps_trvsl_02 + ps_trvsl_03 + ps_trvsl_04 + ps_trvsl_05 +
+             ps_trvsl_06 + ps_trvsl_07 + ps_trvsl_08 + ps_trvsl_09 + ps_trvsl_10
+           )
